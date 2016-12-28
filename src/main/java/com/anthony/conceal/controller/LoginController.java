6f50763c24fcbb24b5e687dfd1fbcbc44ab2ddd0
Anthony <chendong239@pingan.com.cn>
2016-12-28 18:08:19 +0800

    1

--- src/main/java/com/anthony/conceal/controller/LoginController.java
+++ src/main/java/com/anthony/conceal/controller/LoginController.java
@@ -1,14 +1,16 @@
 package com.anthony.conceal.controller;
 
 import com.anthony.conceal.dao.UserMapper;
+import com.anthony.conceal.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -18,16 +20,26 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @Controller
 @RequestMapping("/login")
 public class LoginController {
-
     @Autowired
     private UserMapper userMapper;
+    @RequestMapping(value = "login.do", method = POST)
+    @ResponseBody
+    public Map<String, String> userLogin(@RequestBody UserDTO userDTO) {
+        String pwd = userMapper.userLogin(userDTO.getUserName());
+        String message;
+        if (null == pwd) {
+            //没有用户
+            message = "不存在用户" + userDTO.getUserName();
+        } else {
+            if (pwd.equals(userDTO.getPassWord())) {
+                message = "ok";
+            } else {
+                message = "密码错误";
+            }
+        }
+        Map<String, String> res = new HashMap<>();
+        res.put("res", message);
 
-    @RequestMapping(value = "login.do",method = POST)
-    public String userLogin(HttpServletRequest request)
-    {
-        String userName=request.getParameter("username");
-        String passWord=request.getParameter("password");
-        System.out.println(userMapper.userLogin(userName));
-        return "/test";
+        return res;
     }
 }
--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -2,24 +2,41 @@
  * Created by Anthony on 2016/12/27.
  */
 
-var loginFunc = function()
-{
+var loginFunc = function () {
     $.ajax({
         cache: true,
         type: "POST",
-        url:"login/login.do",
-        data:{
-            username:$('#username').val(),
-            password:$('#password').val()
+        url: "login/login.do",
+        contentType : 'application/json',
+        data: {
+            userName: $('#username').val(),
+            passWord: $('#password').val()
         },
         async: false,
-        success: function(data) {
-            alert(data);
-            $("#commonLayout_appcreshi").parent().html(data);
-        },
-        error: function(request) {
-            alert("Connection error");
-        }
+        success: function (data) {
+            allPrpos(data);
 
+            // $("#commonLayout_appcreshi").parent().html(data);
+        },
+        error: function (request) {
+            allPrpos(request);
+        }
     });
+}
+
+function allPrpos(obj) {
+    // 用来保存所有的属性名称和值
+    var props = "";
+    var test="";
+    // 开始遍历
+    for (var p in obj) { // 方法
+        if (typeof ( obj [p]) == " function ") {
+            obj [p]();
+        } else { // p 为属性名称，obj[p]为对应属性的值
+            props += p + " = " + obj [p] + " /n ";
+            test +=obj [p];
+        }
+    } // 最后显示所有的属性
+    alert(props);
+    alert(test);
 }
