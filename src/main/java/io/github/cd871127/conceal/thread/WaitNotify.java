ba136881cdfddb9c233c23e3ca33310a63fdf5f1
anthony <cdistc@163.com>
2019-04-09 21:15:51 +0800

    1

--- src/main/java/io/github/cd871127/conceal/thread/WaitNotify.java
+++ src/main/java/io/github/cd871127/conceal/thread/WaitNotify.java
@@ -1,8 +1,19 @@
 package io.github.cd871127.conceal.thread;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class WaitNotify implements Runnable {
     @Override
     public void run() {
 
+    }
+
+    public static void main(String[] args) {
+        Lock lock=new ReentrantLock();
+        Condition condition1=lock.newCondition();
+        Condition condition2=lock.newCondition();
+        System.out.println(condition1==condition2);
     }
 }
